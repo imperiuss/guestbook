7c647af7dd63d597b2821e7c6a61a1b5414996ee
Imp <imp@imp.com>
2017-05-20 19:08:56 +0300

    rebuild to own pagination

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,72 +1,54 @@
-var app = angular.module('app', ['ui.grid','ui.grid.pagination']);
-app.controller('RecordCtrl', ['$scope','RecordService', '$http',
-    function ($scope, RecordService, $http) {
+var app = angular.module('app', []);
+app.controller('RecordsController', ['$scope', '$http','RecordService', function ($scope, $http, RecordService) {
+        $scope.records = [];
         var paginationOptions = {
             pageNumber: 1,
-            pageSize: 10,
-            sort: null
+            pageSize: 5
         };
 
-        RecordService.getRecords(
-            paginationOptions.pageNumber,
-            paginationOptions.pageSize).success(function(data){
-            $scope.gridOptions.data = data.content;
-            $scope.gridOptions.totalItems = data.totalElements;
+        RecordService.getRecords(paginationOptions.pageNumber, paginationOptions.pageSize).success(function (data) {
+            $scope.records = data.content;
         });
 
-        $scope.gridOptions = {
-            paginationPageSize: paginationOptions.pageSize,
-            enablePaginationControls: false,
-            enableColumnMenus:false,
-            useExternalPagination: true,
-            columnDefs: [
-                { name: 'name' },
-                { name: 'message' },
-                { name: 'email' },
-                { name: 'date', type: 'date', cellFilter: 'date:\'yyyy-MM-dd h:mma\''}
-            ],
-            onRegisterApi: function(gridApi) {
-                $scope.gridApi = gridApi;
-                gridApi.pagination.on.paginationChanged(
-                    $scope,
-                    function (newPage, pageSize) {
-                        paginationOptions.pageNumber = newPage;
-                        paginationOptions.pageSize = pageSize;
-                        RecordService.getRecords(newPage,pageSize)
-                            .success(function(data){
-                                $scope.gridOptions.data = data.content;
-                                $scope.gridOptions.totalItems = data.totalElements;
-                            });
-                    });
-            }
+        $scope.nextPage = function nextPage() {
+            paginationOptions.pageNumber = paginationOptions.pageNumber + 1;
+            RecordService.getRecords(paginationOptions.pageNumber, paginationOptions.pageSize).success(function (data) {
+                $scope.records = data.content;
+            });
+        };
+        $scope.prevPage = function prevPage() {
+            paginationOptions.pageNumber = paginationOptions.pageNumber - 1;
+            RecordService.getRecords(paginationOptions.pageNumber, paginationOptions.pageSize).success(function (data) {
+                $scope.records = data.content;
+            });
         };
         $scope.addRowAsyncAsJSON = function(){
 
-            var dataObj = {
-                name : $scope.name,
-                message : $scope.message,
-                email : $scope.email,
-                date : new Date()
-            };
-            var res = $http.post('/saverecord', dataObj);
-            res.success(function(data, status, headers, config){
-                $scope.gridOptions.data.unshift(dataObj);
-            });
-            res.error(function(data, status, headers, config) {
-                alert( "failure message: " + JSON.stringify({data: data}));
-            });
-            $scope.name='';
-            $scope.message='';
-            $scope.email='';
+        var dataObj = {
+            name : $scope.name,
+            message : $scope.message,
+            email : $scope.email,
+            date : new Date()
         };
+        var res = $http.post('/saverecord', dataObj);
+        res.success(function(data, status, headers, config){
+            $scope.records.unshift(dataObj);
+        });
+        res.error(function(data, status, headers, config) {
+            alert( "failure message: " + JSON.stringify({data: data}));
+        });
+        $scope.name='';
+        $scope.message='';
+        $scope.email='';
+    };
     }]);
-app.service('RecordService',['$http', function ($http) {
+app.service('RecordService', ['$http', function ($http) {
 
-    function getRecords(pageNumber,size) {
-        pageNumber = pageNumber > 0?pageNumber - 1:0;
+    function getRecords(pageNumber, size) {
+        pageNumber = pageNumber > 0 ? pageNumber - 1 : 0;
         return $http({
             method: 'GET',
-            url: '/records?page='+pageNumber+'&size='+size
+            url: '/records?page=' + pageNumber + '&size=' + size
         });
     }
     return {
